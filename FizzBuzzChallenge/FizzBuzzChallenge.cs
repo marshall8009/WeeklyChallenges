c0305e6ce73ce48c185e742004c73ab1e1e0ae66
Jake <marshall_8009@yahoo.com>
2019-09-10 20:58:25 -0700

    FizzBuzz Challenge added

---
+++ FizzBuzzChallenge/FizzBuzzChallenge.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FizzBuzzChallenge
+{
+    public class FizzBuzzChallenge
+    {
+        public static string Fizzbuzz(int number)
+        {
+            string output = "";
+            for (int i = 1; i <= number; i++)
+            {
+                bool isFizz = (i % 3 == 0);
+                bool isBuzz = (i % 5 == 0);
+
+                if (isFizz && isBuzz)
+                {
+                    output += $"FizzBuzz {i}\n";
+                }
+                else if (isBuzz)
+                {
+                    output += $"Buzz {i}\n";
+                }
+                else if (isFizz)
+                {
+                    output += $"Fizz {i}\n";
+                }
+                else
+                {
+                    output += $"{i}\n";
+                }
+            }
+
+            return output;
+        }
+    }
+}
---
+++ FizzBuzzChallenge/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FizzBuzzChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FizzBuzzChallenge.Fizzbuzz(100));
+
+            Console.ReadLine();
+        }
+    }
+}
