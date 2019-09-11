ab9864a9151e48f79afce00fbdea8876aaa9d3e1
Jake <marshall_8009@yahoo.com>
2019-09-11 11:05:57 -0700

    Linq Challenge

---
+++ LinqChallenge/LinqChallenge.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LinqChallenge
+{
+    public class LinqChallenge
+    {
+        public static void LettersInOrder(string input)
+        {
+            var letters = from x in input
+                          orderby x.ToString()
+                          select x;
+
+            foreach (var letter in letters)
+            {
+                Console.WriteLine(letter);
+            }
+        }
+
+        public static void LettersInAnonymousObject(string input)
+        {
+            var letters = from x in input
+                          group x by x.ToString().ToLower() into y
+                          orderby y.Count() descending, y.Key.ToString()
+                          select (new { Letter = y.Key, Count = y.Count() });
+
+            foreach (var letter in letters)
+            {
+                Console.WriteLine($"Letter: { letter.Letter }, Count: { letter.Count }");
+            }
+        }
+    }
+}
---
+++ LinqChallenge/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LinqChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinqChallenge.LettersInOrder("Hello World");
+            Console.WriteLine();
+            LinqChallenge.LettersInAnonymousObject("Last Hello World");
+
+            Console.ReadLine();
+        }
+    }
+}
