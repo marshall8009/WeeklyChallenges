38e616fe11b6f9e5b35f7cbf58177cc433261900
Jake <marshall_8009@yahoo.com>
2019-10-07 17:06:59 -0700

    Adding extension method challenge

---
+++ ExtensionMethodChallenge/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ExtensionMethodChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName = "Jake Marshall.";
+            "Hello World".Print();
+            myName.Print();
+
+            myName.Excite().Print();
+
+            double output = 4.15.Add(5).Subtract(2).MultilyBy(8).DivideBy(3).Add(34);
+            Console.WriteLine(output);
+
+            PersonModel person = new PersonModel();
+
+            person.Fill().Print();
+
+            Console.ReadLine();
+        }
+    }
+}
